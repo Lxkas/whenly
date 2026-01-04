572f4c33ad349a94fc907355040b1619a4ec0a93
Lxkas <alohasunshineday@yahoo.com>
2026-01-04 12:06:10 +0700

    client/server supabase clients and types

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,19 @@
 import { createBrowserClient } from "@supabase/ssr";
+import { useMemo } from "react";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
 
-export const createClient = () => createBrowserClient(supabaseUrl!, supabaseKey!);
+let client: ReturnType<typeof createBrowserClient> | undefined;
+
+function getSupabaseBrowserClient() {
+    if (client) return client;
+    client = createBrowserClient(supabaseUrl, supabaseKey);
+    return client;
+}
+
+export function useSupabaseBrowser() {
+    return useMemo(getSupabaseBrowserClient, []);
+}
+
+export const createClient = () => createBrowserClient(supabaseUrl, supabaseKey);
